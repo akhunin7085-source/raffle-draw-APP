59cf67bd31b3f53ec6ee5bcbb1f77cb895bb3ac3
akhunin7085-source <a.khunin7085@gmail.com>
2025-12-03 12:00:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -428,7 +428,7 @@ def main():
             padding-right: 5rem;
         }}
         .main .block-container {{
-            max-width: 1300px;
+            max-width: 2000px;
             margin-left: auto;
             margin-right: auto;
             background-color: rgba(14, 17, 23, 0.9);
@@ -616,4 +616,5 @@ if __name__ == '__main__':
             st.session_state.draw_history = []
 
     main()
+
 
