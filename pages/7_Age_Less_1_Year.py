fab0988d39965cd2f67aed811f4e8eb08a8fbc01
akhunin7085-source <a.khunin7085@gmail.com>
2025-12-20 14:02:46 +0700

    Update 7_Age_Less_1_Year.py

--- pages/7_Age_Less_1_Year.py
+++ pages/7_Age_Less_1_Year.py
@@ -64,8 +64,8 @@ def main():
             min-height: 150px;
         }
         .card-prize { color: #ffeb3b; font-size: 1.3em; font-weight: bold; margin-bottom: 10px; display: block; }
-        .card-name { color: #4beaff; font-size: 1.5em; font-weight: bold; }
-        .card-detail { color: #c9c9c9; font-size: 0.9em; margin-top: 5px; }
+        .card-name { color: #4beaff; font-size: 1.2em; font-weight: bold; }
+        .card-detail { color: #c9c9c9; font-size: 1.2em; margin-top: 5px; }
         .card-rank-corner {
             position: absolute;
             right: 15px;
@@ -114,3 +114,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
